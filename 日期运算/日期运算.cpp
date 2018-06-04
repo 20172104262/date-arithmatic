80cc7a95a3cdcaedc7489c8a9175f5ca781b32e8
20172104262 <39482046+20172104262@users.noreply.github.com>
2018-06-04 17:41:38 +0800

    Initial commit

---
+++ 日期运算/日期运算.cpp
@@ -0,0 +1,67 @@
+// 日期运算.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include"iostream"
+using namespace std;
+
+class Cdate
+{
+public:
+	int year;
+	int month;
+	int day;
+	void import(int y,int m,int d);
+	void arithmetic();
+	void display();
+};
+void Cdate::import(int y, int m, int d)
+{
+	year = y;
+	month = m;
+	day = d;
+}
+void Cdate::arithmetic()
+{
+	int a[13] = { 0,31,28,31,30,31,30,31,31,30,31,30,31 };
+	if (year % 400 == 0 || year % 4 == 0 && year%100!=0)
+	{
+		a[2] = 29;
+	}
+	if (day < a[year])
+	{
+		day += 1;
+	}
+	else if (day = a[year])
+	{
+		if (month < 12)
+		{
+			month += 1;
+			day = 1;
+		}
+		else
+		{
+			year += 1;
+			month = 1;
+			day = 1;
+		}
+	}
+	
+}
+void Cdate::display()
+{
+	cout << year << month << day << endl;
+}
+int main()
+{
+	cout << "请输入日期" << endl;
+	Cdate oc;
+	int q, w, e;
+	cin >> q >> w >> e;
+	oc.import(q,w,e);
+	oc.arithmetic();
+	oc.display();
+
+    return 0;
+}
+
